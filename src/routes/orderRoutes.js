56c065614b477ae1e45ec21669dae281162239e7
Vinicius Silva <viniguerras@hotmail.com>
2025-08-14 14:05:16 -0300

    Update orderRoutes.js

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -3,10 +3,14 @@ const router = express.Router();
 const orderController = require('../controllers/orderController');
 const { protect } = require('../middlewares/authMiddleware');
 
-// O cliente precisa estar logado para criar um pedido
+// Rota para o cliente criar um novo pedido e preferência de pagamento
+// A função no controller deve ser 'createOrderAndPreference' ou 'createPagarmeOrder'
 router.post('/', protect, orderController.createOrderAndPreference);
-router.get('/api/orders/my-history', protect, orderController.getMyOrderHistory); // <-- ROTA NOVA
-router.get('/:id', protect, orderController.getOrderDetails); // <-- ADICIONE ESTA LINHA
-// ...
+
+// Rota para o cliente buscar seu histórico de pedidos
+router.get('/my-history', protect, orderController.getMyOrderHistory);
+
+// Rota para o cliente ou motoboy buscar os detalhes de um pedido específico
+router.get('/:id', protect, orderController.getOrderDetails);
 
 module.exports = router;
