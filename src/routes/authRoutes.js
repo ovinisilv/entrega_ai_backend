782f2ee4e7a53b68a7556bdb97099b870986f347
Vinicius Silva <viniguerras@hotmail.com>
2025-08-15 13:26:12 -0300

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-router.post('/google', authController.googleSignIn); // <-- ADICIONE ESTA LINHA
+router.post('/api/register', authController.register);
+router.post('/api/login', authController.login);
+router.post('/api/google', authController.googleSignIn); // <-- ADICIONE ESTA LINHA
 
 module.exports = router;
